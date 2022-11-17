f0e1a2bfdec503ccc85100498316c5b6c75f324a
Kura <me@kurabloodlust.eu>
2022-11-17 06:56:07 +0100

    net standard fix

---
+++ src/Z0ne.Collections/IsExternalInit.cs
@@ -0,0 +1,15 @@
+// ExternalInit.cs Copyright (c) z0ne.
+// All Rights Reserved.
+// Licensed under the EUPL 1.2 License.
+// See LICENSE the project root for license information.
+
+#if NETSTANDARD
+
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices;
+
+internal static class IsExternalInit
+{
+}
+
+#endif
